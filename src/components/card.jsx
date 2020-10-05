f0d7fdd63d5584f63a5b28c4a1cb74a77e2fc7a2
Eslam Hasanen <Eslamspot@gmil.com>
2020-10-05 10:23:10 -0400

    Prepare app for pwa, works 100%

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Cart = ({ id, name, email }) => {
   return (
     <div className="tc bg-light-green bid br4 pa4 grow bw2  fl w-25 ma2 ">
-      <img alt="something" src={`https://robohash.org/${id}?200x200`} />
+      <img alt="something" src={`https://robohash.org/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
